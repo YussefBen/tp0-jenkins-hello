b81637bfcc1f44ae751f3f4e7ce9e95a362dbf39
BEN CHOUCHANE Youssef <yussef.ben@email.com>
2025-12-02 14:01:51 +0100

    Ajout du script calculatrice.py

---
+++ calculatrice.py
@@ -0,0 +1,15 @@
+def addition(a, b):
+    return a + b
+
+def soustraction(a, b):
+    return a - b
+
+def multiplication(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    x = 10
+    y = 5
+    print(f"Addition: {addition(x, y)}")
+    print(f"Soustraction: {soustraction(x, y)}")
+    print(f"Multiplication: {multiplication(x, y)}")
